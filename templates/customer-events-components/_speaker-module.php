e78f2388265de708c303aea6819a03c48702ba24
John Bolos <johnbolos23@gmail.com>
2026-08-21 15:13:38 +0800

    updated analysts or advisor popup logic for the forms

--- templates/customer-events-components/_speaker-module.php
+++ templates/customer-events-components/_speaker-module.php
@@ -179,7 +179,13 @@
                                                 </span>                                               
                                             </div>
                                             <span class="speaker-button-container">
-                                                <span class="std-button form-popup-button-container red-button" style="padding: 0;"><?php echo get_field( 'speaker_form_button', 'options' ); ?></span>
+                                                <span class="std-button form-popup-button-container red-button" style="padding: 0;">
+                                                    <?php if( has_term('adapt-analysts', 'expertise') ) : ?>
+                                                        <a href="<?= get_field( 'speaker_form_button', 'options' ); ?>?which_advisors_are_you_interested_in_meeting=<?= get_the_title(); ?>">Submit an Analyst Enquiry</a>
+                                                    <?php elseif( has_term('adapt-advisors', 'expertise') ) : ?>
+                                                        <a href="<?= get_field( 'speaker_form_button_advisor', 'options' ); ?>?which_advisors_are_you_interested_in_meeting=<?= get_the_title(); ?>">Submit an Advisor Enquiry</a>
+                                                    <?php endif; ?>
+                                                </span>
                                                 <span style="display:none"><?php echo get_field( 'speaker_form_script', 'options' ); ?></span>
                                             </span>
                                         </div>
